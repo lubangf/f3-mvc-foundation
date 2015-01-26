8d32e7f1e2e404e7a51e79744a2dc8eef0a729a4
Giovanni Colapinto <giovanni.colapinto@gmail.com>
2015-01-26 12:15:13 +0100

    Added foundation, monolog and layout

--- app/main/controllers/Controller.php
+++ app/main/controllers/Controller.php
@@ -5,7 +5,10 @@ use \BaseController;
 use \Template;
 
 class Controller extends BaseController {
-	function index($app){}
+	function index($app){
+        $app->set('page_title','Home');
+        $app->set('inc','index.html');
+    }
 
 	function page1($app){}
 
--- core/BaseController.php
+++ core/BaseController.php
@@ -1,4 +1,8 @@
 <?php
+require_once 'vendor/autoload.php';
+use \Monolog\Logger;
+use \Monolog\Handler\StreamHandler;
+use \Monolog\Handler\RotatingFileHandler;
 
 class BaseController {
 	/** @var \Base $app */
@@ -8,17 +12,30 @@ class BaseController {
 
 	function __construct(){
 		$this->app = Base::instance();
-		$module = $this->app->get("PARAMS.module");
-		$module = $module ? $module : $this->app->get("DefaultModule");
-		$this->app->concat('UI',';app/'.$module.'/views/');
-		$this->app->set('LOCALES', 'app/' . $module . '/dict/'); // load module dictionary
+        $module = $this->app->get("PARAMS.module");
+        $module = $module ? $module : $this->app->get("DefaultModule");
+        $session = $this->app->get("SESSION");
+        if(isset($session['message'])){
+            $this->app->set('message', $session['message']);
+            $this->app->set('message_severity', $session['message_severity']);
+            $this->app->set('message_text', $session['message_text']);
+        }
+        $this->logger = new Logger('Georeport');
+        $this->logger->pushHandler(new RotatingFileHandler($this->app->get('log_file'), $this->app->get('log_level')));
+        $this->app->concat('UI',';app/'.$module.'/views/');
+        $this->app->set('LOCALES', 'app/' . $module . '/dict/'); // load module dictionary
 	}
 
 	function afterroute(){
 		if($this->render){
-			$param = $this->app->get('PARAMS');
-			$action = empty($param['action'])?'index':$param['action'];
-			echo Template::instance()->render($action.".html");
-		}
+            $param = $this->app->get('PARAMS');
+            $action = empty($param['action'])?'index':$param['action'];
+            echo Template::instance()->render("layout.html");
+        }
+        $session = $this->app->get("SESSION");
+        $session['message'] = False;
+        $session['message_severity'] = '';
+        $session['message_text'] = '';
+        $this->app->set('SESSION', $session);
 	}
 }
